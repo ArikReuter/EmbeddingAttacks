6967841293d6b7bab4269d8f647e26f36c1fec76
MrDomani <pawel.morgen@pm.me>
2024-01-20 17:48:27 +0100

    ignore case in regex

--- data/reddit/extract_sample_utterances.sh
+++ data/reddit/extract_sample_utterances.sh
@@ -1,4 +1,4 @@
-unzstd -c RC_2019-04.zst | grep -F -f control_users_sample_patterns.txt > control_users_utterances.njson
-unzstd -c RC_2019-04.zst | grep -F -f sensitive_users_sample_patterns.txt > sensitive_users_utterances.njson
-unzstd -c RS_2019-04.zst | grep -F -f control_users_sample_patterns.txt >> control_users_utterances.njson
-unzstd -c RS_2019-04.zst | grep -F -f sensitive_users_sample_patterns.txt >> sensitive_users_utterances.njson
+unzstd -c RC_2019-04.zst | grep -Fi -f control_users_sample_patterns.txt > control_users_utterances.njson
+unzstd -c RC_2019-04.zst | grep -Fi -f sensitive_users_sample_patterns.txt > sensitive_users_utterances.njson
+unzstd -c RS_2019-04.zst | grep -Fi -f control_users_sample_patterns.txt >> control_users_utterances.njson
+unzstd -c RS_2019-04.zst | grep -Fi -f sensitive_users_sample_patterns.txt >> sensitive_users_utterances.njson
--- data/reddit/extract_users.sh
+++ data/reddit/extract_users.sh
@@ -1,2 +1,2 @@
 REGEX='"author":"[^"]+"'
-unzstd -c RS_2019-04.zst | tee >(grep -oE $REGEX > all_authors.txt) | grep -F -f sensitive-subreddit-patterns.txt | grep -oE $REGEX > sensitive_authors.txt
+unzstd -c RS_2019-04.zst | tee >(grep -oE $REGEX > all_authors.txt) | grep -Fi -f sensitive-subreddit-patterns.txt | grep -oE $REGEX > sensitive_authors.txt
